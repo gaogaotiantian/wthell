c6fd01a2bb90ec7315902fb0e541f235f6f2b91d
gaogaotiantian <gaogaotiantian@hotmail.com>
2020-09-01 21:43:52 -0700

    version 0.0.3

--- src/wthell/__init__.py
+++ src/wthell/__init__.py
@@ -2,9 +2,9 @@ import sys
 from .wthell import WTHell
 
 
+__version__ = "0.0.3"
+
 if "wth" not in locals():
     wth = WTHell()
     sys.excepthook = wth.excepthook
     sys.settrace(wth.tracefunc)
-
-__version__ = "0.0.2"
